bb93766e15e5116fc0a06efeb1e9266a09643fab
BlasAdayMedinaGonzalez <usr2702@salesianos-lacuesta.net>
2022-02-01 10:41:05 +0000

    Fix: refactor unnecessary code

--- src/com/adaysoft/client/MainClient.java
+++ src/com/adaysoft/client/MainClient.java
@@ -5,9 +5,9 @@ import java.io.ObjectOutputStream;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.Scanner;
-
+@SuppressWarnings("unchecked")
 public class MainClient {
-
+    
     public static void main(String[] args) {
 
         try (Socket s = new Socket("localhost", 8080);
@@ -35,9 +35,8 @@ public class MainClient {
                 oos.writeObject(msg);
 
                 String msgServer = (String) ois.readObject();
-                if (msgServer != null) {
-                    System.out.println(msgServer);
-                }
+                System.out.println(msgServer);
+                
             }
             
         } catch (Exception e) {
